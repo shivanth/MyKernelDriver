57007893027fdc57e3db72345ef5a45cc4462533
Shivanth <fordearlinux@gmail.com>
2010-10-29 02:06:45 +0530

    implemented read and write

--- mine.c
+++ mine.c
@@ -20,12 +20,13 @@ ssize_t memory_write(struct file*filp,char * buff,size_t count,loff_t * f_pos);
 void memory_exit();
 
 int memory_init();
-
+/*The file ops structure definition for my driver Currently dealing only with open close read add write 
+ */
 struct file_operations memory_fops={
  read:memory_read,
  write:memory_write,
  open:memory_open,
- release_memory_close};
+ release:memory_close};
 }
 
 int major_number=0;
@@ -41,13 +42,10 @@ int __init module_start_up(){
   buffer=kmalloc(1,GFP_KERNEL);
   if(!buffer){
     result=-ENOMEM;
-    memory_exit();
+    memory_close();
     return result;
   }
   memset(buffer,0,1);
-  
- 
-
   return 0;
 }
 void memory_close(){
@@ -55,6 +53,19 @@ void memory_close(){
   if(buffer)
     kfree(buffer);
 }
+ssize_t memory_read(struct file * filep,char * buff,size_t count,loff_t * f_pos){
+  if(!buff)
+    return -ENOMEM;//don't ask me why i really need to think about this return value
+  int k=copy_to_user(buffer,buff,1);
+  return k;
+}
+ssize_t memory_write(struct file * filep,char * buff,size_t count,loff_t * f_pos){
+  if(!buff)
+    return -ENOMEM;// again food for thought
+  int k=copy_from_user(buffer,buff,1);
+  return k;
+}
+
 void __exit module_stop(){
   printk(KERN_INFO "Goof Bye world");
 }
