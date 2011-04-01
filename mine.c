7de78d864caa3532ded277a453e0d0a00d46a5c2
Shivanth <fordearlinux@gmail.com>
2011-04-01 11:08:43 +0530

    Updated mine.c

--- mine.c
+++ mine.c
@@ -1,4 +1,3 @@
-
 #include<linux/module.h>
 #include<linux/kernel.h>
 #include<linux/init.h>
@@ -7,24 +6,23 @@
 #include<linux/types.h>
 #include<asm/types.h>
 #include<asm/uaccess.h>
-
+#include<linux/cdev.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
+
+#define MAJOR_NO 12
 
 
 
 int memory_open(struct inode *inode,struct file*filp);
-
 void  memory_close();
-
 ssize_t memory_read(struct file * filep,char* buff,size_t count,loff_t* f_pos);
-
 ssize_t memory_write(struct file*filp,char * buff,size_t count,loff_t * f_pos);
 
 void memory_exit();
 
 int memory_init();
-/*The file ops structure definition for my driver Currently dealing only with open close read add write 
+/*The file ops structure definition for my driver Currently dealing only with open close read and write 
  */
 struct file_operations memory_fops={
  read:memory_read,
@@ -33,46 +31,63 @@ struct file_operations memory_fops={
  release:memory_close
 };
 
-
-int major_number=0;
-
-char *  buffer;
+struct mydev {
+  dev_t devno;
+  int maximum;
+  char * buffer;
+  struct cdev mycdev;
+  size_t size;
+}mydev;
 
 int __init module_start_up(){
-  int result=register_chrdev(major_number,"Mydevice",&memory_fops);
+  mydev.devno=MKDEV(MAJOR_NO,12);
+  cdev_init(&(mydev.mycdev),&memory_fops);
+  int result=cdev_add(&(mydev.mycdev),mydev.devno,12);
   if(result<0){
     printk(KERN_ALERT "Error registering device");
     return result;
 }
-  buffer=kmalloc(1,GFP_KERNEL);
-  if(!buffer){
+  mydev.maximum=1024*1024;
+  mydev.size=0;
+  mydev.buffer=kmalloc(mydev.maximum,GFP_KERNEL);
+  if(!mydev.buffer){
     result=-ENOMEM;
     memory_close();
     return result;
   }
-  memset(buffer,0,1);
+  memset(mydev.buffer,0,mydev.maximum);
   return 0;
 }
 void memory_close(){
-  unregister_chrdev(major_number,"Mydevice");
-  if(buffer)
-    kfree(buffer);
+  cdev_del(&(mydev.mycdev));
+  if(mydev.buffer)
+    kfree(mydev.buffer);
 }
 ssize_t memory_read(struct file * filep,char * buff,size_t count,loff_t * f_pos){
-  if(!buff)
+  int copysize;
+  if(buff)
     return -ENOMEM;//don't ask me why i really need to think about this return value
-  int k=copy_to_user(buffer,buff,1);
+  if(mydev.size>count )
+     copysize=count;
+  else 
+    copysize=mydev.size;
+  int k=copy_to_user(mydev.buffer,buff,copysize);
   return k;
 }
 ssize_t memory_write(struct file * filep,char * buff,size_t count,loff_t * f_pos){
   if(!buff)
     return -ENOMEM;// again food for thought
-  int k=copy_from_user(buffer,buff,1);
+  if(count+mydev.size>mydev.maximum)
+    return -ENOMEM;
+  int k=copy_from_user(mydev.buffer,buff,count);
   return k;
 }
-
+int memory_open(struct inode * inode ,struct  file * filep){
+  return 0;
+}
 void __exit module_stop(){
   printk(KERN_INFO "Goof Bye world");
+  memory_close();
 }
 
 module_init(module_start_up);
